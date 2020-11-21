b0fecf79607bcd7a66ea49bb08524413e23de8b3
Khalid Othman <khalid0edu@gmail.com>
2020-11-21 20:28:07 +0800

    - Adopted Object-Oriented paradigm. - Parents selection & offspring mutation is probabilistic now.

--- qga.py
+++ qga.py
@@ -1,15 +1,89 @@
-from random import choices, randint
+from random import choices, random
 from statistics import mean
 from typing import List
 from tabulate import tabulate
-from tkinter import Tk, Button, Label, Text, StringVar, LEFT, RIGHT,X, Y, BOTH, END
+from tkinter import Tk, Button, Label, Text, END
 import pandas
 import matplotlib.pyplot as plt
 
 
 # Global Types
-Chromosome = List[int]
-Population = List[Chromosome]
+class Chromosome:
+    def __init__(self, genes_list: List[int] = []):
+        self.genes = genes_list.copy()
+        self.calculate_fitness()
+    
+    # For chromosomes comparison
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Chromosome):
+            return NotImplemented
+        return self.genes == other.genes
+    
+    # Chromosome Fitness Calculation
+    def calculate_fitness(self) -> None:
+        self.fitness = 0
+        for gene in self.genes:
+            self.fitness += gene
+    
+    # Normalizes the Chromosome Fitness
+    def normalize_fitness(self, total_population_fitness: int):
+        self.normalized_fitness = self.fitness/total_population_fitness
+
+class Population:
+    def __init__(self, chrome_list: List[Chromosome] = []) -> None:
+        self.chromosomes = chrome_list.copy()
+    
+    # Creates Random Population
+    def random_population(self) -> None:
+        for i in range(POPULATION_SIZE):
+            self.chromosomes.append(Chromosome(choices([0, 1], k=LENGTH)))
+        self.calculate_fitness()
+    
+    # Calculate the normalized fitness of the population
+    def calculate_fitness(self) -> None:
+        self.fitness_array = []     # Flushing the array
+        for chrome in self.chromosomes:
+            self.fitness_array.append(chrome.fitness)
+        total_fitness = sum(self.fitness_array)
+        for chrome in self.chromosomes:
+            chrome.normalize_fitness(total_fitness)
+    
+    # Parents Selection
+    def select_parents(self) -> List[Chromosome]:
+        parents: List[Chromosome] = []
+        parent1 = choices(self.chromosomes, weights=self.fitness_array, k=1)[0]
+        parents.append(parent1)
+        parent2 = choices(self.chromosomes, weights=self.fitness_array, k=1)[0]
+        while parent1 == parent2:   # ensuring parents are separate individual
+            parent2 = choices(self.chromosomes, weights=self.fitness_array, k=1)[0]
+        parents.append(parent2)
+        return parents
+
+    # Crossover
+    def crossover(self, parents: List[Chromosome]) -> List[Chromosome]:
+        children = []
+        child1 = Chromosome(parents[0].genes[0:10] + parents[1].genes[10:])
+        child2 = Chromosome(parents[1].genes[0:10] + parents[0].genes[10:])
+        children.append(child1)
+        children.append(child2)
+        return children
+    
+    # Randomly Mutate Genes on the Chromosome
+    def mutate(self, child: Chromosome) -> None:
+        for i in range(LENGTH):
+            if random() < 0.1:      # Each gene has a 10% probability of mutating
+                child.genes[i] = 0 if child.genes[i] == 1 else 1
+        child.calculate_fitness()
+
+    # Eliminating the Weakest Chromosomes from the population
+    def eliminate(self) -> None:
+        self.chromosomes.pop(self.fitness_array.index(min(self.fitness_array)))
+    
+    # Add new chromosome to the population
+    def add(self, chrome: Chromosome) -> None:
+        self.eliminate()
+        self.chromosomes.append(chrome)
+        self.calculate_fitness()
 
 # Constants
 LENGTH = 20     # Length of each chromosome
@@ -22,71 +96,9 @@ generations_stats = pandas.DataFrame(columns=['Generation', 'Min', 'Max', 'Avera
 window = Tk()
 txt = Text(window, width=40)
 
-# Population Creation
-def random_population() -> Population:
-    pop = []
-    for i in range(POPULATION_SIZE):
-        pop.append(choices([0, 1], k=LENGTH))
-    return pop
-
-# Chromosome Fitness Calculation
-def chromosome_fitness(chrome: Chromosome) -> int:
-    fitness = 0
-    for gene in chrome:
-        fitness += gene
-    return fitness
-
 # Population Fitness Calculations
 def population_fitness_stats(epoch: int, pop: Population) -> None:
-    fit_array = []
-    for chrome in pop:
-        fit_array.append(chromosome_fitness(chrome))
-    generations_stats.loc[len(generations_stats)] = [epoch, min(fit_array), max(fit_array), mean(fit_array)]    # Append to the end of the dataframe
-    #TODO normalization
-
-# Parents Selection
-def select_parents(population: Population) -> Population:
-    pop = population.copy()     # to avoid modifying the original list referenced in the func parameter
-    fit_array = []
-    parents = []
-    for chrome in pop:
-        fit_array.append(chromosome_fitness(chrome))
-
-    max_fitness = max(fit_array)
-    indices = []
-    for i in range(len(fit_array)):
-        if fit_array[i] == max_fitness:
-            indices.append(i)
-    if len(indices) > 1:
-        for i in range(2):
-            parents.append(pop[indices[i]])
-        return parents
-    else:
-        parents.append(pop[indices[0]])
-        pop.pop(indices[0])
-        fit_array.pop(indices[0])
-        parents.append(pop[fit_array.index(max(fit_array))])
-        return parents
-
-# Crossover
-def crossover(parents: Population) -> Population:
-    children = []
-    children.append(parents[0][0:10] + parents[1][10:])
-    children.append(parents[1][0:10] + parents[0][10:])
-    return children
-
-# Randomly Mutate Genes on the Chromosome
-def mutate(child: Chromosome) -> None:
-    locuses = choices([i for i in range(20)], k=3)      # Locations of the genes to mutate on the chromosome. k = number of genes to mutate
-    for i in locuses:
-        child[i] = 0 if child[i] == 1 else 1
-
-# Eliminating the Weakest
-def eliminate(population: Population) -> None:
-    fit_array = []
-    for chrome in population:
-        fit_array.append(chromosome_fitness(chrome))
-    population.pop(fit_array.index(min(fit_array)))
+    generations_stats.loc[len(generations_stats)] = [epoch, min(pop.fitness_array), max(pop.fitness_array), mean(pop.fitness_array)]    # Append to the end of the dataframe
 
 # Plotting the Stats
 def plot_stats() -> None:
@@ -108,18 +120,18 @@ def plot_stats() -> None:
 
 # Running the simulation
 def run_sim() -> None:
-    pop = random_population()
+    pop = Population()
+    pop.random_population()
     global generations_stats
     generations_stats = pandas.DataFrame(columns=['Generation', 'Min', 'Max', 'Average'])   # Flushing the dataframe from any previous results
     for i in range(0, MAX_GENERATION):
         population_fitness_stats(i+1, pop)
-        parents = select_parents(pop)
-        children = crossover(parents)
-        mutate(children[0])
-        mutate(children[1])
-        pop.extend(children)
-        eliminate(pop)
-        eliminate(pop)
+        parents = pop.select_parents()
+        children = pop.crossover(parents)
+        pop.mutate(children[0])
+        pop.mutate(children[1])
+        pop.add(children[0])
+        pop.add(children[1])
 
         # Termination condition
         if generations_stats.iloc[i, 2] == 20:
@@ -134,6 +146,7 @@ def run_sim() -> None:
 # Show a Scatterplot of min, max and average Population Fitness over the Generations
 def gui() -> None:
     window.geometry('+400+100')
+    window.title('Binary String Problem')
     txt.grid(rowspan=3, columnspan=2, sticky='nsew')
     btn1 = Button(window, text='Run', command=run_sim)
     btn1.grid(row=1, column=2, sticky='nsew')
